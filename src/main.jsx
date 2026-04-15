3116f3a512c6a93bba466c3e2987bd4765ee4c61
Jeandedie <168834153+Jeandedie@users.noreply.github.com>
2026-04-15 16:54:49 +0000

    first commit
    Removed routing functionality and related imports from App component.

--- src/main.jsx
+++ src/main.jsx
@@ -2,23 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-export default App;
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
